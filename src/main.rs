6837610ae9bc2c1c79cbff70c44afd2c060ac078
muvlon <muvlon@hentzes.de>
2018-01-19 23:40:27 +0100

    rename "printer" to "output" in main for clarity

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use action::graph::*;
 
 fn main() {
     let mut graph = Graph::new();
-    let sine    = graph.insert_module(Sine::with_freq(220.0));
-    let printer = graph.insert_module(PortAudioOut::default());
+    let sine   = graph.insert_module(Sine::with_freq(220.0));
+    let output = graph.insert_module(PortAudioOut::default());
 
-    graph.connect((sine, 0), (printer, 0)).unwrap();
+    graph.connect((sine, 0), (output, 0)).unwrap();
 
     loop { graph.run() };
 }
